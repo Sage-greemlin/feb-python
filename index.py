a4bc2facbf5a4a7d2794b6fa0b86624d5e72ca33
Patrick <patrickhood963@gmail.com>
2025-03-24 23:19:25 +0200

    Creating a list and manipulating it for Week Two Assignment

--- index.py
+++ index.py
@@ -12,10 +12,13 @@ print("The first language is: ", languages[0]) # The first language is:  Python
 print("The second language is: ", languages[1]) # The second language is:  Java
 print("The third language is: ", languages[2])  # The third language is:  C++
 print("The fourth language is: ", languages[3])  # The fourth language is:  JavaScript
-print("The fifth language is: ", languages[4])
+print("The fifth language is: ", languages[4]) # The fith language is: PHP
 
 #multiple data types to multiple variables
-name, age, country = str(input("enter name")), int(input("enter age")), str(input("enter country"))
-print("My name is: ", name) # My name is:  Patrick
-print("I am ", age, " years old") # I am  22  years old 
-print("I am from: ", country) # I am from:  South Afric
+#name, age, country = str(input("enter name")), int(input("enter age")), str(input("enter country"))
+#print("My name is: ", name) # My name is:  Patrick
+#print("I am ", age, " years old") # I am  22  years old 
+#print("I am from: ", country) # I am from:  South Africa
+
+print(type(languages))
+print(type(num1))
